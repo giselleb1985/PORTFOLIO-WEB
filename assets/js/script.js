e52eb8a9dd6eb4e7e46183ad0fd2894b10dea264
giselleb1985 <100442048+giselleb1985@users.noreply.github.com>
2022-08-18 08:52:40 -0300

    my changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,8 @@ window.addEventListener("resize", anchoPage);
 var formulario_login = document.querySelector(".formulario__login");
 var formulario_register = document.querySelector(".formulario__register");
 var contenedor_login_register = document.querySelector(".contenedor__login-register");
-var caja_trasera_login = document.querySelector(".caja__trasera-login");
-var caja_trasera_register = document.querySelector(".caja__trasera-register");
+var caja_trasera_login = document.querySelector(".caja_trasera-login");
+var caja_trasera_register = document.querySelector(".caja_trasera-register");
 
     //FUNCIONES
 
